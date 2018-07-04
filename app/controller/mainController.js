5cb33e0239c533954a27e1e019c3e02e031fc064
Shang, Neil(China - Chengdu) <neilshang@aaxischina.com>
2018-07-03 22:12:07 -0400

    update readme

--- app/controller/mainController.js
+++ app/controller/mainController.js
@@ -2,14 +2,9 @@
 const Controller = require('egg').Controller;
 class CustomController extends Controller {
     async custonIndex() {
-        let id =  await this.ctx.query.id;
+        let id =   this.ctx.query.id;
         let str = await this.ctx.service.seckill.show(id);
         this.ctx.body = 'this is my controller'+str;
     }
-    async getParam(){
-
-        this.ctx.body = id;
-    }
 }
-
 module.exports = CustomController;
